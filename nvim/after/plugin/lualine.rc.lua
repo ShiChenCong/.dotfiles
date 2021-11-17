f322e26ad35ec381147d73155c97d7be2b0a542a
chencong <chencong@dian.so>
2021-11-17 21:27:39 +0800

    change lualine theme

--- nvim/after/plugin/lualine.rc.lua
+++ nvim/after/plugin/lualine.rc.lua
@@ -5,7 +5,8 @@ lualine.setup {
   options = {
     icons_enabled = true,
     -- theme = 'solarized_dark',
-    theme = onedark,
+    -- theme = onedark,
+    theme = 'gruvbox',
     section_separators = {left = '', right = ''},
     component_separators = {left = '', right = ''},
     disabled_filetypes = {}
