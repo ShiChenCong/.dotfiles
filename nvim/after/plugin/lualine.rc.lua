59e0074db15fb7c5d8ffee5b62ad247d839cc5e5
chencong <chencong@dian.so>
2021-12-24 15:07:33 +0800

    change font

--- nvim/after/plugin/lualine.rc.lua
+++ nvim/after/plugin/lualine.rc.lua
@@ -8,6 +8,7 @@ lualine.setup {
     -- theme = onedark,
     -- theme = 'gruvbox',
     -- theme = 'codedark',
+    theme = 'onedark-nvim',
     section_separators = {left = '', right = ''},
     component_separators = {left = '', right = ''},
     disabled_filetypes = {}
