25065cf36c6ba309747f8ad06d254899f7eaafc0
chencong <chencong@dian.so>
2021-12-25 17:23:19 +0800

    add match word color

--- nvim/after/plugin/gitsign.rc.lua
+++ nvim/after/plugin/gitsign.rc.lua
@@ -57,3 +57,14 @@ require('gitsigns').setup {
 }
  
 vim.cmd("hi GitSignsCurrentLineBlame guifg=#4e5362")
+vim.cmd("highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080")
+vim.cmd("highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6")
+vim.cmd("highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6")
+vim.cmd("highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE")
+vim.cmd("highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE")
+vim.cmd("highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE")
+vim.cmd("highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0")
+vim.cmd("highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0")
+vim.cmd("highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4")
+vim.cmd("highlight! CmpItemKindProperty guibg=NONE guifg=#D4D4D4")
+vim.cmd("highlight! CmpItemKindUnit guibg=NONE guifg=#D4D4D4")
