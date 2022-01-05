e2aea141767288521dc0bde6eaed313399e1951d
chencong <chencong@dian.so>
2022-01-05 14:41:11 +0800

    change diffview height

--- nvim/after/plugin/diffview.rc.lua
+++ nvim/after/plugin/diffview.rc.lua
@@ -15,7 +15,7 @@ require'diffview'.setup {
   file_panel = {
     position = "left",            -- One of 'left', 'right', 'top', 'bottom'
     width = 35,                   -- Only applies when position is 'left' or 'right'
-    height = 10,                  -- Only applies when position is 'top' or 'bottom'
+    height = 8,                  -- Only applies when position is 'top' or 'bottom'
     listing_style = "tree",       -- One of 'list' or 'tree'
     tree_options = {              -- Only applies when listing_style is 'tree'
       flatten_dirs = true,
@@ -24,8 +24,8 @@ require'diffview'.setup {
   },
   file_history_panel = {
     position = "bottom",
+    height = 8,
     width = 35,
-    height = 16,
     log_options = {
       max_count = 256,      -- Limit the number of commits
       follow = false,       -- Follow renames (only for single file)
