be767261f76569077a03ef00fb5d6e113f2d249a
chencong <chencong@dian.so>
2022-03-27 11:13:20 +0800

    change config

--- nvim/after/plugin/commom_setup.rc.lua
+++ nvim/after/plugin/commom_setup.rc.lua
@@ -1,32 +1,30 @@
 require('Comment').setup({
-     ---@param ctx Ctx
-    -- pre_hook = function(ctx)
-    --     -- Only calculate commentstring for tsx filetypes
-    --     if vim.bo.filetype == 'typescriptreact' then
-    --         local U = require('Comment.utils')
-    --
-    --         -- Detemine whether to use linewise or blockwise commentstring
-    --         local type = ctx.ctype == U.ctype.line and '__default' or '__multiline'
-    --
-    --         -- Determine the location where to calculate commentstring from
-    --         local location = nil
-    --         if ctx.ctype == U.ctype.block then
-    --             location = require('ts_context_commentstring.utils').get_cursor_location()
-    --         elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-    --             location = require('ts_context_commentstring.utils').get_visual_start_location()
-    --         end
-    --
-    --         return require('ts_context_commentstring.internal').calculate_commentstring({
-    --             key = type,
-    --             location = location,
-    --         })
-    --     end
-    -- end,
     pre_hook = function(ctx)
       return require('ts_context_commentstring.internal').calculate_commentstring()
     end,
 })
+local status_ok, comment = pcall(require, "Comment")
+if not status_ok then
+  return
+end
 
+comment.setup {
+  pre_hook = function(ctx)
+    local U = require "Comment.utils"
+
+    local location = nil
+    if ctx.ctype == U.ctype.block then
+      location = require("ts_context_commentstring.utils").get_cursor_location()
+    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+      location = require("ts_context_commentstring.utils").get_visual_start_location()
+    end
+
+    return require("ts_context_commentstring.internal").calculate_commentstring {
+      key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+      location = location,
+    }
+  end,
+}
 require('onedark').setup({
   dark_float = true
 })
--- nvim/after/plugin/treesitter.rc.lua
+++ nvim/after/plugin/treesitter.rc.lua
@@ -1,15 +1,16 @@
 require'nvim-treesitter.configs'.setup {
   context_commentstring = {
     enable = true,
-     config = {
-      javascript = {
-        __default = '// %s',
-        jsx_element = '{/* %s */}',
-        jsx_fragment = '{/* %s */}',
-        jsx_attribute = '// %s',
-        comment = '// %s'
-      }
-    }
+    enable_autocmd = false,
+    -- config = {
+    --   javascript = {
+    --     __default = '// %s',
+    --     jsx_element = '{/* %s */}',
+    --     jsx_fragment = '{/* %s */}',
+    --     jsx_attribute = '// %s',
+    --     comment = '// %s'
+    --   }
+    -- }
   },
   autotag = {
     enable = true,
