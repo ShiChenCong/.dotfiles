9cf3ae4f488063b5c2ab1fc50801abb628e3fb0f
chencong <chencong@dian.so>
2022-05-14 17:28:32 +0800

    add alias

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,10 @@ alias t "tmux"
 alias ta "tmux attach"
 alias lg 'lazygit'
 alias cl 'clear'
+alias j 'z'
+alias ls "exa --icons -l"
+alias lsa "ll -a"
+
 
 set -gx PATH /opt/homebrew/bin $PATH
 set -gx PATH /opt/homebrew/Cellar/node@14/14.17.6/bin:$PATH 
