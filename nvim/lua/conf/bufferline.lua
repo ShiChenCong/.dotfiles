4ebb1e0d301052437bf639a8366a7afaaaa959c5
chencong <chencong@dian.so>
2022-05-14 22:05:23 +0800

    no message

--- nvim/lua/conf/bufferline.lua
+++ nvim/lua/conf/bufferline.lua
@@ -28,7 +28,7 @@ require("bufferline").setup {
     max_name_length = 18,
     max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
     tab_size = 18,
-    diagnostics = "nvim_lsp",
+    diagnostics = false,
     diagnostics_update_in_insert = false,
     diagnostics_indicator = function(count, level, diagnostics_dict, context)
       local s = ""
