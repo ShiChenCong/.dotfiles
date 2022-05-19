940dc1f67ea441d749611a02c5c71bb1b92bc0ea
chencong <chencong@dian.so>
2022-05-19 18:43:38 +0800

    change config

--- nvim/lua/conf/cmp.lua
+++ nvim/lua/conf/cmp.lua
@@ -64,7 +64,7 @@ local cmp_kinds = {
 
 cmp.setup({
   window = {
-    completion = cmp.config.window.bordered(),
+    -- completion = cmp.config.window.bordered(),
     documentation = cmp.config.window.bordered(),
   },
   snippet = {
