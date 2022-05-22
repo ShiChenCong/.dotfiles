f3b83a194a1de39221bb7a923713310ba5d424e9
chencong <chencong@dian.so>
2022-05-22 21:12:39 +0800

    no message

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -13,6 +13,33 @@ vim.cmd [[
   augroup luaFormat
     autocmd BufWritePre *.lua lua vim.lsp.buf.format()
   augroup end
+
+  " 新一行 不带上行的注释
+  augroup newlineWithoutComment
+    autocmd BufEnter * set fo-=c fo-=r fo-=o
+  augroup end
+
+  "defx 映射;
+  augroup defxMap
+    autocmd FileType defx nnoremap <buffer> ; :
+  augroup end
+
+  augroup CursorLine
+    au!
+    au VimEnter * setlocal cursorline
+    au WinEnter * setlocal cursorline
+    au BufWinEnter * setlocal cursorline
+    au WinLeave * setlocal nocursorline
+  augroup END
+
+  augroup termOpenInsert
+    autocmd TermOpen * startinsert
+  augroup END
+
+  " no numbers in term mode
+  augroup termNoNumber
+    au TermOpen * setlocal nonumber norelativenumber nospell
+  augroup END
 ]]
 
 vim.cmd [[
@@ -27,35 +54,10 @@ vim.cmd [[
   hi! LspReferenceText cterm=bold ctermbg=red guibg=#1c4c5e
   hi! LspReferenceWrite cterm=bold ctermbg=red guibg=#1c4c5e
 
-  " 新一行 不带上行的注释
-  autocmd BufEnter * set fo-=c fo-=r fo-=o
-
-  "defx 映射;
-  autocmd FileType defx nnoremap <buffer> ; :
-
-  augroup CursorLine
-    au!
-    au VimEnter * setlocal cursorline
-    au WinEnter * setlocal cursorline
-    au BufWinEnter * setlocal cursorline
-    au WinLeave * setlocal nocursorline
-  augroup END
-
   hi DiagnosticUnderlineWarn gui=undercurl
   hi DiagnosticUnderlineError gui=undercurl
   hi DiagnosticUnderlineInfo gui=undercurl
   hi DiagnosticUnderlineHint gui=undercurl
-
-
-
-  augroup termOpenInsert
-    autocmd TermOpen * startinsert
-  augroup END
-
-  " no numbers in term mode
-  augroup termNoNumber
-    au TermOpen * setlocal nonumber norelativenumber nospell
-  augroup END
 ]]
 
 vim.api.nvim_create_augroup("lsp_document_highlight", { clear = false })
