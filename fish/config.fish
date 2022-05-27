27628e89d52f44abbdf7e7ae4002962f9b4f5541
chencong <chencong@dian.so>
2022-05-27 22:27:56 +0800

    add rust path

--- fish/config.fish
+++ fish/config.fish
@@ -16,5 +16,6 @@ alias ssr "http_proxy=http://127.0.0.1:7890 https_proxy=http://127.0.0.1:7890"
 set -gx PATH /opt/homebrew/bin $PATH
 set -gx PATH /opt/homebrew/Cellar/node@14/14.17.6/bin:$PATH
 # set -gx PATH /usr/local/bin/node:$PATH
+set -gx PATH "$HOME/.cargo/bin" $PATH
 
 set -U fish_greeting ""
