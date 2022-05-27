8a583ebda096d425bf96b5285f1a568bc6c295e6
chencong <chencong@dian.so>
2022-05-27 22:37:57 +0800

    change config

--- nvim/lua/conf/trouble.lua
+++ nvim/lua/conf/trouble.lua
@@ -7,5 +7,5 @@ require('trouble').setup({
 
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>Trouble<cr>', opts)
-vim.api.nvim_set_keymap('n', '[f', '<cmd>lua require("trouble").previous({ skip_groups = true, jump = false })<CR>', opts)
-vim.api.nvim_set_keymap('n', ']f', '<cmd>lua require("trouble").next({ skip_groups = true, jump = false })<CR>', opts)
+vim.api.nvim_set_keymap('n', '[f', ':lua require("trouble").previous({ skip_groups = true, jump = true })<CR>', opts)
+vim.api.nvim_set_keymap('n', ']f', ':lua require("trouble").next({ skip_groups = true, jump = true })<CR>', opts)
