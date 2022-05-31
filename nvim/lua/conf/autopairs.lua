04db4421e88fe313e1ca08bfe943331056c3069a
chencong <chencong@dian.so>
2022-05-31 19:14:29 +0800

    add nvim-autopairs fast wrap map

--- nvim/lua/conf/autopairs.lua
+++ nvim/lua/conf/autopairs.lua
@@ -1,5 +1,5 @@
 require('nvim-autopairs').setup({
-  disable_filetype = { "TelescopePrompt"},
+  disable_filetype = { "TelescopePrompt" },
 })
 
 -- If you want insert `(` after select function or method item
@@ -7,3 +7,22 @@ require('nvim-autopairs').setup({
 -- local cmp = require('cmp')
 -- cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
 
+-- put this to setup function and press <a-e> to use fast_wrap
+local npairs = require("nvim-autopairs")
+npairs.setup({
+  fast_wrap = {},
+})
+
+-- change default fast_wrap
+npairs.setup({
+  fast_wrap = {
+    map = '<C-e>',
+    chars = { '{', '[', '(', '"', "'" },
+    pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+    end_key = '$',
+    keys = 'qwertyuiopzxcvbnmasdfghjkl',
+    check_comma = true,
+    highlight = 'Search',
+    highlight_grey = 'Comment'
+  },
+})
