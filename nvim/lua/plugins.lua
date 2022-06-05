45e91efcb5116fa7609174b7b83cef3e28ee7c96
chencong <chencong@dian.so>
2022-06-05 17:49:55 +0800

    add git-conflit-plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -124,5 +124,15 @@ return packer.startup({
       'kyazdani42/nvim-tree.lua'
     }
     use { 'mhartington/formatter.nvim' }
+    use { 'akinsho/git-conflict.nvim', config = function()
+      require('git-conflict').setup({
+        default_mappings = true, -- disable buffer local mapping created by this plugin
+        disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+        highlights = { -- They must have background color, otherwise the default color will be used
+          incoming = 'DiffText',
+          current = 'DiffAdd',
+        }
+      })
+    end }
   end,
 })
