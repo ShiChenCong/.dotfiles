65db1c5400d1fca6957b99302d132c8a10fa0e7a
chencong <chencong@dian.so>
2022-06-15 11:18:38 +0800

    change color

--- nvim/lua/conf/lualine.lua
+++ nvim/lua/conf/lualine.lua
@@ -1,17 +1,44 @@
 local status, lualine = pcall(require, "lualine")
 if (not status) then return end
-local onedark = require 'lualine.themes.onedark'
+-- local onedark = require 'lualine.themes.onedark'
+local cp = require("catppuccin.core.palettes.init").get_palette()
+local catppuccin = {}
 
+catppuccin.normal = {
+  a = { bg = cp.blue, fg = cp.mantle, gui = "bold" },
+  b = { bg = cp.surface1, fg = cp.blue },
+  c = { bg = cp.mantle, fg = cp.text },
+}
+
+catppuccin.insert = {
+  a = { bg = cp.green, fg = cp.base, gui = "bold" },
+  b = { bg = cp.surface1, fg = cp.teal },
+}
+
+catppuccin.command = {
+  a = { bg = cp.peach, fg = cp.base, gui = "bold" },
+  b = { bg = cp.surface1, fg = cp.peach },
+}
+
+catppuccin.visual = {
+  a = { bg = cp.mauve, fg = cp.base, gui = "bold" },
+  b = { bg = cp.surface1, fg = cp.mauve },
+}
+
+catppuccin.replace = {
+  a = { bg = cp.red, fg = cp.base, gui = "bold" },
+  b = { bg = cp.surface1, fg = cp.red },
+}
+
+catppuccin.inactive = {
+  a = { bg = cp.mantle, fg = cp.blue },
+  b = { bg = cp.mantle, fg = cp.surface1, gui = "bold" },
+  c = { bg = cp.mantle, fg = cp.overlay0 },
+}
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = onedark,
-    -- theme = 'onenord',
-    -- theme = 'solarized_dark',
-    -- theme = 'gruvbox',
-    -- theme = 'codedark',
-    -- theme = 'onedark-nvim',
-    -- theme = "nightfly",
+    theme = catppuccin,
     globalstatus = true,
     section_separators = { left = '', right = '' },
     component_separators = { left = '', right = '' },
