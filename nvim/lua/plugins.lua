1494fe8a625da894e7ec5ee374d161c222765819
chencong <chencong@dian.so>
2022-06-21 18:09:39 +0800

    remove autp-session

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -173,16 +173,16 @@ return packer.startup({
     -- use "rafamadriz/friendly-snippets"
 
     use "ThePrimeagen/harpoon"
-    use {
-      'rmagatti/auto-session',
-      config = function()
-        require('auto-session').setup {
-          log_level = 'info',
-          auto_session_suppress_dirs = { '~/', '~/Projects' }
-        }
-        vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
-      end
-    }
+    -- use {
+    --   'rmagatti/auto-session',
+    --   config = function()
+    --     require('auto-session').setup {
+    --       log_level = 'info',
+    --       auto_session_suppress_dirs = { '~/', '~/Projects' }
+    --     }
+    --     vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
+    --   end
+    -- }
     -- use 'famiu/bufdelete.nvim'
     use 'lewis6991/impatient.nvim'
     -- use 'vimpostor/vim-tpipeline'
