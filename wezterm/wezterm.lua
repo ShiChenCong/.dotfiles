c94a40765af56ca112d82853e283d30b925b49dd
chencong <chencong@dian.so>
2022-08-13 16:44:04 +0800

    add wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require('wezterm')
+local config = {
+  font_size = 20,
+  line_height = 1.2,
+  font = wezterm.font("JetBrainsMono Nerd Font Mono"),
+  window_decorations = "RESIZE",
+  hide_tab_bar_if_only_one_tab = true,
+  color_scheme = 'Whimsy',
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  }
+}
+
+return config
