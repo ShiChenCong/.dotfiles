d66c9b74aa7659df1c65a0e4074176b6c61a764a
chencong <chencong@dian.so>
2022-08-21 20:58:40 +0800

    add html snip

--- nvim/lua/conf/luasnip.lua
+++ nvim/lua/conf/luasnip.lua
@@ -50,6 +50,25 @@ ls.add_snippets(nil, {
       t '] = useState(',
       i(2),
       t ')'
-    })
+    }),
   },
+  html = {
+    s("html", fmt([[
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Document</title>
+        </head>
+
+        <body>
+          {}
+        </body>
+
+        </html>
+    ]], { i(0) }))
+  }
 })
