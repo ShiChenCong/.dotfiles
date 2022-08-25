d789d8c4a6658801ac1394e33a36e62444a3d5e7
chencong <chencong@dian.so>
2022-08-25 23:31:33 +0800

    change eslint config

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -51,12 +51,29 @@ for _, lsp in ipairs(servers) do
     root_dir = function()
       return vim.fn.getcwd()
     end,
-    -- 修复eslint 报错https://neovim.discourse.group/t/supressing-eslint-ls-errors/1687
-    handlers = {
-      ['window/showMessageRequest'] = function(_, result, params) return result end
-    }
   }
 end
+
+nvim_lsp.eslint.setup {
+  on_attach = function()
+    vim.api.nvim_create_augroup('AutoFormatAndFixEslint', { clear = true })
+    vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+      group = 'AutoFormatAndFixEslint',
+      pattern = { "*.tsx", "*.ts", "*.js" },
+      callback = function()
+        vim.cmd [[EslintFixAll]]
+      end
+    })
+  end,
+  capabilities = capabilities,
+  root_dir = function()
+    return vim.fn.getcwd()
+  end,
+  -- 修复eslint 报错https://neovim.discourse.group/t/supressing-eslint-ls-errors/1687
+  handlers = {
+    ['window/showMessageRequest'] = function(_, result, params) return result end
+  }
+}
 
 local signs = {
   { name = "DiagnosticSignError", text = ' ', texthl = 'DiagnosticSignError' },
