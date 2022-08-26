50ee8b4267e658bcc0e93ec96eef9abf61d6341b
chencong <chencong@dian.so>
2022-08-26 14:29:57 +0800

    bufferline custom hidden

--- nvim/lua/conf/bufferline.lua
+++ nvim/lua/conf/bufferline.lua
@@ -1,65 +1,18 @@
 vim.opt.termguicolors = true
 require("bufferline").setup {
-  highlights = {
-    -- buffer_selected = {
-    --   guifg = '#ced1d8',
-    --   guibg = '#282C34',
-    -- },
-  },
   options = {
-    close_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-    right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-    left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
-    middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
-    -- NOTE: this plugin is designed with this icon in mind,
-    -- and so changing this is NOT recommended, this is intended
-    -- as an escape hatch for people who cannot bear it for whatever reason
-    -- indicator_icon = '',
-    buffer_close_icon = '',
-    modified_icon = '●',
-    close_icon = '',
-    left_trunc_marker = '',
-    right_trunc_marker = '',
-    name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
-      if buf.name:match('%.md') then
-        return vim.fn.fnamemodify(buf.name, ':t:r')
-      end
-    end,
-    max_name_length = 18,
-    max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
-    tab_size = 20,
-    diagnostics = false,
-    diagnostics_update_in_insert = false,
-    diagnostics_indicator = function(count, level, diagnostics_dict, context)
-      local s = ""
-      for e, n in pairs(diagnostics_dict) do
-        local sym = e == "error" and " "
-            or (e == "warning" and " " or " ")
-        s = s .. n .. sym
-      end
-      return s
-    end,
-    BufferLinePickClosecustom_filter = function(buf_number)
-      -- filter out filetypes you don't want to see
-      if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
-        return true
-      end
-      -- filter out by buffer name
-      if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
-        return true
-      end
-      -- filter out based on arbitrary rules
-      -- e.g. filter out vim wiki buffer from tabline in your work repo
-      if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
-        return true
-      end
-    end,
     offsets = { { filetype = "NvimTree", text = "File Explorer", text_align = "left" } },
-    show_buffer_icons = true,
-    show_buffer_close_icons = true,
-    show_close_icon = true,
-    show_tab_indicators = true,
-    persist_buffer_sort = true,
-    separator_style = "slant"
+    separator_style = "slant",
+    custom_filter = function(buf_number, buf_numbers)
+      local buf_name = vim.fn.bufname(buf_number)
+      if string.find(buf_name, 'fugitive') then
+        return false
+      end
+
+      if vim.bo[buf_number].filetype == 'qf' then
+        return false
+      end
+      return true
+    end
   }
 }
