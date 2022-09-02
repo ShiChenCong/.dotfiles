42424ccad2ce24c3368b654d9ab1e0e0e3ce9516
chencong <chencong@dian.so>
2022-09-02 19:00:50 +0800

    no message

--- nvim/lua/conf/formatter.lua
+++ nvim/lua/conf/formatter.lua
@@ -6,7 +6,16 @@ require('formatter').setup {
     --   require('formatter.filetypes.lua').stylua,
     -- },
     typescriptreact = {
-      require('formatter.filetypes.typescriptreact').prettierd
+      -- require('formatter.filetypes.typescriptreact').prettierd
+      -- eslint_d  more fast than eslintfixall
+      require('formatter.filetypes.typescriptreact').eslint_d
+      -- function()
+      --   return {
+      --     exe = "prettierd",
+      --     args = { vim.api.nvim_buf_get_name(0) },
+      --     stdin = true
+      --   }
+      -- end
     },
     typescript = {
       require('formatter.filetypes.typescriptreact').prettierd
