d10ac9def0363f79edbf5ffeefd0a36f61e6b599
chencong <chencong@dian.so>
2022-09-26 18:56:56 +0800

    chore: fix nvim-ufo error

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -35,7 +35,8 @@ local on_attach = function(client, bufnr)
   end
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.foldingRange = {
   dynamicRegistration = false,
   lineFoldingOnly = true
