f61f509b9084bdd98bb02457fa19f7547ac97c43
chencong <chencong@dian.so>
2022-12-07 17:39:34 +0800

    change config

--- nvim/lua/conf/treesitter.lua
+++ nvim/lua/conf/treesitter.lua
@@ -20,16 +20,16 @@ require 'nvim-treesitter.configs'.setup {
   autotag = {
     enable = true,
   },
-  -- highlight = {
-  --   enable = true,
-  --   disable = function(lang, buf)
-  --     local max_filesize = 100 * 1024 -- 100 KB
-  --     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-  --     if ok and stats and stats.size > max_filesize then
-  --       return true
-  --     end
-  --   end,
-  -- },
+  highlight = {
+    enable = true,
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
+  },
   ensure_installed = {
     'css',
     'typescript',
