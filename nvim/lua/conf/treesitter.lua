cd8b45e67eef77c65322edac799ec3e97bad8fbf
chencong <chencong@dian.so>
2022-12-07 17:47:17 +0800

    change config

--- nvim/lua/conf/treesitter.lua
+++ nvim/lua/conf/treesitter.lua
@@ -44,3 +44,8 @@ require 'nvim-treesitter.configs'.setup {
 
 local ft_to_parser = require "nvim-treesitter.parsers".filetype_to_parsername
 ft_to_parser.less = "scss" -- the someft filetype will use the python parser and queries.
+
+local timer = vim.loop.new_timer()
+timer:start(30, 0, vim.schedule_wrap(function()
+  vim.cmd("e")
+end))
