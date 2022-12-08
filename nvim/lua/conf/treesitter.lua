824ee2a4bcacb50ea50589cc68efc8018580c117
chencong <chencong@dian.so>
2022-12-08 10:43:54 +0800

    change config

--- nvim/lua/conf/treesitter.lua
+++ nvim/lua/conf/treesitter.lua
@@ -1,4 +1,47 @@
-require 'nvim-treesitter.configs'.setup {}
+require 'nvim-treesitter.configs'.setup {
+  indent = {
+    enable = true,
+    disable = { 'rust' },
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "g.",
+      node_incremental = "g.",
+      -- scope_incremental = "grc",
+      -- node_decremental = "grm",
+    },
+  },
+  -- 配合nvim-ts-context-commentstring插件
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
+  autotag = {
+    enable = true,
+  },
+  highlight = {
+    enable = true,
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
+  },
+  ensure_installed = {
+    'css',
+    'typescript',
+    'tsx',
+    'javascript',
+    'html',
+    'lua',
+    'json',
+    'rust'
+  }
+}
 
 local ft_to_parser = require "nvim-treesitter.parsers".filetype_to_parsername
 ft_to_parser.less = "scss" -- the someft filetype will use the python parser and queries.
+
