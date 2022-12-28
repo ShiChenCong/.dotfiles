153a1eb0694efe397099e71358f9e18af37d4e16
chencong <chencong@dian.so>
2022-12-28 14:27:47 +0800

    change config

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,10 +3,9 @@ vim.g.mapleader = " "
 if not vim.g['vscode'] then
   require('options')
   require('keymaps')
-  --require('plugins')
   require('plugins2')
-  require('autocmd')
   require('theme')
+  require('autocmd')
 else
   require('vscode.keymaps')
 end
