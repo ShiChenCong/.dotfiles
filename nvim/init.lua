7fe62055ac19272615d73cce0411a802ee0260b0
chencong <chencong@dian.so>
2022-12-28 12:01:46 +0800

    change config

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,8 +4,8 @@ if not vim.g['vscode'] then
   require('options')
   require('theme')
   require('keymaps')
-  --require('plugins')
-  require('plugins2')
+  require('plugins')
+  --require('plugins2')
   require('autocmd')
 else
   require('vscode.keymaps')
