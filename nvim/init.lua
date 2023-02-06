10bdae22cc29990e0f0ad3de0b1450e49b9d0358
chencong <chencong@dian.so>
2023-02-06 18:45:19 +0800

    remove plugins2

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = " "
 if not vim.g['vscode'] then
   require('options')
   require('keymaps')
-  require('plugins2')
+  require('plugins')
   require('theme')
   require('autocmd')
 else
