f4eaeecdf7905a1557d98bced578dcbd5ba82cd3
chencong <sccong@outlook.com>
2023-04-01 13:22:56 +0800

    add wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,14 @@
-function getMacModel()
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+
+-- maximize when open
+wezterm.on("gui-startup", function()
+  local tab, pane, window = mux.spawn_window {}
+  window:gui_window():maximize()
+end)
+
+-- 判断当前是哪台电脑
+local function getMacModel()
   local f = io.popen("/usr/sbin/system_profiler SPHardwareDataType")
   local info = f:read("*all")
   f:close()
@@ -12,28 +22,27 @@ function getMacModel()
   end
 end
 
-local wezterm = require('wezterm')
 local config = {
-    font_size = getMacModel() == 'Mac mini9' and 24 or 17,
-    line_height = 1.1,
-    font = wezterm.font("JetBrains Mono"),
-    window_decorations = "RESIZE",
-    hide_tab_bar_if_only_one_tab = true,
-    color_scheme = 'Whimsy',
-    window_padding = {
-        left = 0,
-        right = 0,
-        top = 0,
-        bottom = 0,
-    },
-    enable_csi_u_key_encoding = true,
-    -- cursor_blink_ease_in = "Constant",
-    -- cursor_blink_ease_out = "Constant",
-    colors = {
-        background = '#25273a'
-    },
-    window_background_opacity = 0.9,
-    macos_window_background_blur = 30
+  font_size = getMacModel() == 'Mac mini9' and 24 or 17,
+  line_height = 1.1,
+  font = wezterm.font("JetBrains Mono"),
+  window_decorations = "RESIZE",
+  hide_tab_bar_if_only_one_tab = true,
+  color_scheme = 'Whimsy',
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+  enable_csi_u_key_encoding = true,
+  -- cursor_blink_ease_in = "Constant",
+  -- cursor_blink_ease_out = "Constant",
+  colors = {
+    background = '#25273a'
+  },
+  window_background_opacity = 0.9,
+  macos_window_background_blur = 30
 }
 
 return config
