a0524318843354f5f7d87cdeb9a272910a84024e
chencong <sccong@outlook.com>
2023-05-31 11:45:28 +0800

    lualine add filepath

--- nvim/lua/conf/lualine.lua
+++ nvim/lua/conf/lualine.lua
@@ -55,7 +55,11 @@ lualine.setup {
     lualine_c = {
       -- '%=',
       -- getTmux
-      -- { 'filename', path = 1 }
+      {
+        'filename',
+        path = 1,
+        color = { fg = '#545970' },
+      }
     },
     lualine_x = {
       -- 'encoding',
