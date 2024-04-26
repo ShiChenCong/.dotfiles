ceb59cdc8af9f9ce34c62843b2fecebea6fe3190
chencong <sccong@outlook.com>
2024-04-26 14:35:46 +0800

    wip

--- fish/config.fish
+++ fish/config.fish
@@ -13,6 +13,7 @@ alias ssr "http_proxy=http://127.0.0.1:1087 https_proxy=http://127.0.0.1:1087"
 alias ff "fzf | xargs nvim"
 
 alias ps "python3 -m http.server"
+alias nv 'neovide --frame buttonless'
 
 alias gs 'git log --pretty=format:"%C(yellow)%h %Cblue%>(12)%ad %Cgreen%<(7)%aN%Cred%d %Creset%s" --graph'
 alias ssr "http_proxy=http://127.0.0.1:7890 https_proxy=http://127.0.0.1:7890"
