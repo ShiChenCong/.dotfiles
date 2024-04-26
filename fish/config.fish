3cedf37bb43120a4acaad6f6c95ad2d7e5174dea
chencong <sccong@outlook.com>
2024-04-26 20:03:39 +0800

    wip

--- fish/config.fish
+++ fish/config.fish
@@ -29,6 +29,7 @@ set -gx PATH /opt/homebrew/Cellar/node@14/14.17.6/bin:$PATH
 # set -gx PATH /usr/local/bin/node:$PATH
 set -gx PATH "$HOME/.cargo/bin" $PATH
 set -g PATH /Users/scc/.local/share/bob/nvim-bin $PATH
+set -g PATH /Users/shichencong/.local/share/bob/nvim-bin $PATH
 
 # deno
 set -x PATH $HOME/.deno/bin:$PATH
