6c9fbd12c622b2da80408e2705740aee081afc40
chencong <sccong@outlook.com>
2024-11-10 10:46:03 +0800

    update

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -62,7 +62,7 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   group = 'formatOnSave2',
   pattern = {
     "*.less,*.css,*.html",
-    "*.tsx,*.jsx", "*.ts", "*.js", "*.lua", "*.rs"
+    "*.tsx,*.jsx", "*.ts", "*.js", "*.lua", "*.rs", "*.json"
   },
   callback = function()
     local line_count = vim.fn.line('$');
@@ -71,7 +71,8 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     if is_node_module_file ~= true and line_count < 700 then
       -- vim.cmd('FormatWrite')
       -- vim.lsp.buf.format({ async = false })
-      vim.lsp.buf.format { filter = function(client) return client.name ~= "ts_ls" end }
+      -- vim.lsp.buf.format { filter = function(client) return client.name ~= "ts_ls" end }
+      vim.lsp.buf.format {}
       -- vim.cmd.write()
     end
   end
