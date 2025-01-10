544360685410024cd764a26f749afef474ab55fd
chencong <sccong@outlook.com>
2025-01-10 11:28:45 +0800

    updat

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -411,17 +411,24 @@ require("lazy").setup({
     "saghen/blink.cmp",
     dependencies = {
       "onsails/lspkind.nvim",
+      {
+        'L3MON4D3/LuaSnip',
+        version = 'v2.*',
+        config = function()
+          require('conf.luasnip')
+        end
+      }
     },
     version = "*",
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-
-      appearance = {
-        use_nvim_cmp_as_default = false,
-        nerd_font_variant = "mono",
-      },
+      snippets = { preset = 'luasnip' },
+      -- appearance = {
+      --   use_nvim_cmp_as_default = false,
+      --   nerd_font_variant = "mono",
+      -- },
 
       completion = {
         accept = { auto_brackets = { enabled = true } },
@@ -434,9 +441,11 @@ require("lazy").setup({
         },
 
         list = {
-          selection = function(ctx)
-            return ctx.mode == "cmdline" and "auto_insert" or "preselect"
-          end,
+          selection = {
+            preselect = function(ctx)
+              return ctx.mode == "cmdline" and "auto_insert" or "preselect"
+            end,
+          }
         },
 
         menu = {
@@ -444,7 +453,7 @@ require("lazy").setup({
 
           cmdline_position = function()
             if vim.g.ui_cmdline_pos ~= nil then
-              local pos = vim.g.ui_cmdline_pos           -- (1, 0)-indexed
+              local pos = vim.g.ui_cmdline_pos -- (1, 0)-indexed
               return { pos[1] - 1, pos[2] }
             end
             local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
@@ -452,31 +461,31 @@ require("lazy").setup({
           end,
 
           draw = {
-            columns = {
-              { "kind_icon", "label", gap = 1 },
-              { "kind" },
-            },
-            components = {
-              kind_icon = {
-                text = function(item)
-                  local kind = require("lspkind").symbol_map[item.kind] or ""
-                  return kind .. " "
-                end,
-                highlight = "CmpItemKind",
-              },
-              label = {
-                text = function(item)
-                  return item.label
-                end,
-                highlight = "CmpItemAbbr",
-              },
-              kind = {
-                text = function(item)
-                  return item.kind
-                end,
-                highlight = "CmpItemKind",
-              },
-            },
+            -- columns = {
+            --   { "kind_icon", "label", gap = 1 },
+            --   { "kind" },
+            -- },
+            -- components = {
+            --   kind_icon = {
+            --     text = function(item)
+            --       local kind = require("lspkind").symbol_map[item.kind] or ""
+            --       return kind .. " "
+            --     end,
+            --     highlight = "CmpItemKind",
+            --   },
+            --   label = {
+            --     text = function(item)
+            --       return item.label
+            --     end,
+            --     highlight = "CmpItemAbbr",
+            --   },
+            --   kind = {
+            --     text = function(item)
+            --       return item.kind
+            --     end,
+            --     highlight = "CmpItemKind",
+            --   },
+            -- },
           },
         },
       },
@@ -517,24 +526,24 @@ require("lazy").setup({
       },
 
       sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-        cmdline = {},     -- Disable sources for command-line mode
-        providers = {
-          lsp = {
-            min_keyword_length = 2,         -- Number of characters to trigger porvider
-            score_offset = 0,               -- Boost/penalize the score of the items
-          },
-          path = {
-            min_keyword_length = 0,
-          },
-          snippets = {
-            min_keyword_length = 2,
-          },
-          buffer = {
-            min_keyword_length = 5,
-            max_items = 5,
-          },
-        },
+        default = { "snippets", "lsp", "path", "buffer" },
+        cmdline = {}, -- Disable sources for command-line mode
+        -- providers = {
+        --   lsp = {
+        --     min_keyword_length = 2, -- Number of characters to trigger porvider
+        --     score_offset = 0,       -- Boost/penalize the score of the items
+        --   },
+        --   path = {
+        --     min_keyword_length = 0,
+        --   },
+        --   snippets = {
+        --     min_keyword_length = 2,
+        --   },
+        --   buffer = {
+        --     min_keyword_length = 5,
+        --     max_items = 5,
+        --   },
+        -- },
       },
     },
   },
