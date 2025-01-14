f97c47e5381d79144c2473e4166323b5e7c270c6
chencong <sccong@outlook.com>
2025-01-14 19:39:15 +0800

    update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -435,6 +435,11 @@ require("lazy").setup({
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono'
       },
+      completion = {
+        documentation = {
+          auto_show = true,
+        }
+      },
 
       keymap = {
         ["<C-e>"] = { "hide", "fallback" },
