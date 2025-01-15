fa2c9c0461bc06f55561e5a03a7671649be9205f
chencong <sccong@outlook.com>
2025-01-15 18:55:52 +0800

    chore: update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -404,9 +404,15 @@ require("lazy").setup({
       {
         'L3MON4D3/LuaSnip',
         version = 'v2.*',
+        dependencies = {
+          'rafamadriz/friendly-snippets',
+          config = function()
+            require('luasnip.loaders.from_vscode').lazy_load({ paths = { vim.fn.stdpath 'config' .. '/snippets' } })
+          end
+        },
         config = function()
           require('conf.luasnip')
-        end
+        end,
       }
     },
     version = "*",
