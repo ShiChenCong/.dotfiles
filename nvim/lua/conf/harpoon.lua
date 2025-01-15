34f41c3abec8bd05dcf29f10c0b0c1988ad9a498
chencong <sccong@outlook.com>
2025-01-15 09:35:13 +0800

    update harpoon

--- nvim/lua/conf/harpoon.lua
+++ nvim/lua/conf/harpoon.lua
@@ -5,18 +5,23 @@ end
 
 local map = require('util.map')
 
-map('n', '<leader>a', ':lua require("harpoon.mark").add_file()<CR>')
-map('n', '<C-e>', ':lua require("harpoon.ui").toggle_quick_menu()<CR>')
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 -- map('n', 'gh', ':lua require("harpoon.ui").nav_file(1)<CR>')
 -- map('n', 'gj', ':lua require("harpoon.ui").nav_file(2)<CR>')
 -- map('n', 'gk', ':lua require("harpoon.ui").nav_file(3)<CR>')
 -- map('n', 'gl', ':lua require("harpoon.ui").nav_file(3)<CR>')
 
 harpoon.setup({
-  menu = {
-    width = vim.api.nvim_win_get_width(0) - 40,
-  },
-  global_settings = {
-    mark_branch = true,
+  settings = {
+    save_on_toggle = true,
+    sync_on_ui_close = true
   }
+  -- menu = {
+  --   width = vim.api.nvim_win_get_width(0) - 40,
+  -- },
+  -- global_settings = {
+  --   mark_branch = true,
+  -- }
 })
+
